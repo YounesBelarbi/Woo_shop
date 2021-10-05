86ad2344329b3a1cbc873d383a43e17caa8ae658
YounesBelarbi <ybelarbi.dev@gmail.com>
2021-10-05 09:24:30 +0200

    feat: check if metabox class exist

--- content/plugins/messagePlugin/messagePlugin.php
+++ content/plugins/messagePlugin/messagePlugin.php
@@ -12,8 +12,6 @@ require plugin_dir_path(__FILE__) . 'includes/class-meta-box.php';
 add_action('add_meta_boxes', ['messagePlugin\MetaBox', 'init_metabox']);
 add_action('save_post', ['messagePlugin\MetaBox', 'save_metabox']);
 
-
-
 if (get_post_meta(9, 'my_meta_box_message_check', true) == 'on') {
 
     add_action('woocommerce_before_account_navigation', ['messagePlugin\MetaBox', 'my_account_text']);
